5af105a153a542eecbe23d7535c8ea640a104ec3
omid kianfar <kianfar.omid.990@gmail.com>
2022-04-23 00:07:36 -0700

    fix validation and design validation

--- src/Components/Validation/TodoValidation.js
+++ src/Components/Validation/TodoValidation.js
@@ -1,23 +1,44 @@
-import { useContext } from "react";
-
+import { useContext, useEffect, useState } from "react";
 import TodoContext from "../../Hooks/TodoContext";
+
+import Styles from "./TodoValidation.module.css";
 
 const TodoValidation = () => {
   const { inputTodo } = useContext(TodoContext);
 
-  let valid = "";
-  if (inputTodo.length >= 85 && inputTodo.length <= 99) {
-    valid = "Its too long chracter. maximum is 100 character";
-  } else if (inputTodo.length == 100) {
-    valid = "Maximum is 100 character";
-  } else {
-    valid = "";
-  }
+  const [warning, setWarning] = useState("");
+  const [error, setError] = useState("");
+
+  let inputTodoLength = inputTodo.length;
+
+  useEffect(() => {
+    if (inputTodoLength >= 130 && inputTodoLength <= 133) {
+      setWarning("Its too long chracter. maximum is 100 character");
+    } else {
+      setWarning("");
+    }
+    if (inputTodoLength == 150) {
+      setError("Maximum is 100 character");
+    } else {
+      setError("");
+    }
+  }, [inputTodoLength]);
 
   return (
-    <div>
-      <p>{valid}</p>
+    <div className={Styles.validBox}>
+      {warning ? (
+        <div className={Styles.warning}>
+          <p>{warning}</p>
+        </div>
+      ) : error ? (
+        <div className={Styles.error}>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
+
 export default TodoValidation;
